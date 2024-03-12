2a1359e936a51c46764499a1d473573820855d93
ismailAchibat <ismailachibat4@gmail.com>
2024-03-12 17:30:42 +0000

    auth layout change

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -9,16 +9,28 @@ const AuthLayout = () => {
         <Navigate to="/" />
       ) : (
         <>
-          <section className="flex flex-1 justify-center items-center flex-col py-10">
+          <section className="flex flex-1 justify-center items-center flex-col py-10 p-10">
             <Outlet />
           </section>
-
           <img
             src="public/assets/images/btig_sideImg2.png"
             alt="logo"
-            className="hidden xl:block border-2 border-gray-800 rounded-lg"
+            className="hidden xl:block md:mr-10 lg:mr-18 xl:mr-28"
           />
         </>
+        /*<>
+        <section class="flex justify-center items-center w-1/2 h-screen">
+          <Outlet />
+        </section>
+      
+        <div class="flex justify-center items-center w-1/2 h-screen">
+          <img
+            src="public/assets/images/btig_sideImg2.png"
+            alt="logo"
+            class="max-w-full max-h-full"
+          />
+        </div>
+      </>*/
       )}
     </>
   );
